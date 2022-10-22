16d5afa290f210bd15692c072ae68e06d3b4382a
urospolovina <uros.polovina@gmail.com>
2022-10-22 14:31:36 +0200

    dodavanje nekog sranja

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,9 +1,10 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 text-4xl font-bold">
+  <div class="text-center text-4xl font-bold">
     <h1>
       Goranin website
     </h1>
+    <h2 class="text-center font-bold">Zasto ovo sranje ne radi</h2>
   </div>
 </template>
 
